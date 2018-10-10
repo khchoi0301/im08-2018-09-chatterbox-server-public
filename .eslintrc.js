a090b9bec69b85005f8a3538752e80fa2839401d
Lee Hoyong <igotoweb@gmail.com>
2018-10-10 10:16:19 +0900

    remove legacy dependencies

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-/**
- * These rules enforce Code States's style guide.
- * Visit this repo for more information:
- *   https://github.com/codestates/eslint-config-codestates
- */
-
-module.exports = {
-  extends: './node_modules/eslint-config-codestates/index.js'
-};
